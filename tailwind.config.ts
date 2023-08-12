762ee97e33cce3eff21eb87845dc926990f12d90
asfiowilma <asfiolithaw@yahoo.co.id>
2023-08-12 19:40:53 +0700

    🎉 initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["src/**/*.{js,jsx,ts,tsx}"],
+  variants: {
+    scrollbar: ["dark", "rounded"],
+  },
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
+};
+export default config;
