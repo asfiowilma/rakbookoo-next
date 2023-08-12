482921ac016d2941ab358256969b9ef0ac3fcef5
asfiowilma <asfiolithaw@yahoo.co.id>
2023-08-12 20:34:00 +0700

    🔧 fix css not loaded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ["src/**/*.{js,jsx,ts,tsx}"],
+  content: ['app/**/*.{js,jsx,ts,tsx}'],
   variants: {
-    scrollbar: ["dark", "rounded"],
+    scrollbar: ['dark', 'rounded'],
   },
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
-};
-export default config;
+  plugins: [require('@tailwindcss/typography'), require('tailwind-scrollbar')],
+}
+export default config
